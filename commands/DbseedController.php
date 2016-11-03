5a5183fc0c9620b3f2cd06a1aa0ff906377b63dd
Sergey Gorobets <abc.studio.gorobets@gmail.com>
2016-11-03 17:58:13 +0200

    add default data in database (topmenu)

---
+++ commands/DbseedController.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\commands;
+
+use yii\console\Controller;
+use Yii;
+
+/**
+ * This command echoes the first argument that you have entered.
+ *
+ * This command is provided as an example for you to learn how to create console commands.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class DbseedController extends Controller
+{
+    /**
+     * This command echoes what you have entered as the message.
+     * @param string $message the message to be echoed.
+     */
+
+    public $sql;
+    public function actionIndex()
+    {
+        $db = Yii::$app->db;
+        $transaction = $db->beginTransaction();
+
+        try {
+            Yii::$app->db->createCommand()->batchInsert('topmenu', ['title'], [
+                ['transport'],
+                ['real_estate'],
+                ['child_world'],
+                ['job'],
+                ['animals'],
+                ['house_garden'],
+                ['electronics'],
+                ['business_and_services'],
+                ['fashion_style'],
+                ['sport'],
+                ['helping'],
+                ['giveAwey'],
+                ['exchange'],
+            ])->execute();
+
+            $transaction->commit();
+
+        } catch(\Exception $e) {
+
+            $transaction->rollBack();
+
+            throw $e;
+        }
+    }
+}
