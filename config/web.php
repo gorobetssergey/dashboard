30e68e3790c4dc689bd16247eace061e3e1adde7
Sergey Gorobets <abc.studio.gorobets@gmail.com>
2016-11-03 13:36:41 +0200

    pretty URL

--- config/web.php
+++ config/web.php
@@ -6,6 +6,9 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language'=>'ru',
+    'sourceLanguage'=>'ru-RU',
+    'timeZone' => 'Europe/Kiev',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -38,14 +41,12 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
