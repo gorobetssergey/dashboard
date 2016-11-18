d13af8063fc945a11067fd5b504d96b1cb6dc86e
Sergey Gorobets <abc.studio.gorobets@gmail.com>
2016-11-18 15:52:27 +0200

    fix

--- components/views/viewItems.php
+++ components/views/viewItems.php
@@ -4,7 +4,7 @@ use yii\helpers\Url;
 <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <h3 class="text-info text-center"><?=array_pop($model->transportProps)->value.'. Стоимость '.$model->transportProps[0]->value?> Грн</h3>
+            <h3 class="text-info text-center"><?=$model->transportProps[count($model->transportProps)-1]->value.'. Стоимость '.$model->transportProps[0]->value?> Грн</h3>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-center" style="overflow: hidden">
             <div class="row">
