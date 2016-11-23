92cd1a2e3a0f650db099e0e2b4b65aa341fc218e
Sergey Gorobets <abc.studio.gorobets@gmail.com>
2016-11-23 18:11:58 +0200

    start menu

--- components/views/menu.php
+++ components/views/menu.php
@@ -5,15 +5,55 @@
 <div class="container-fluid">
     <div class="row">
         <div id = "start-menu" class="col-lg-12">
-            <ul class="menu_start">
-                <li><a href="">ТРАНСПОРТ</a></li>
-                <li><a href="">НЕДВИЖИМОСТЬ</a></li>
-                <li><a href="">ДЕТСКИЙ МИР</a></li>
-                <li><a href="">РАБОТА</a></li>
-                <li><a href="">ЖИВОТНЫЕ</a></li>
-                <li><a href="">ДОМ И САД</a></li>
-                <li><a href="">ЭЛЕКТРОНИКА</a></li>
-            </ul>
+            <div class="btn-group btn-group-justified" role="group" aria-label="...">
+                <div class="btn-group" role="group">
+                    <a href="" type="button" class="btn btn-default menu_start">ТРАНСПОРТ</a>
+                </div>
+                <div class="btn-group" role="group">
+                    <a href="" type="button" class="btn btn-default menu_start">НЕДВИЖИМОСТЬ</a>
+                </div>
+                <div class="btn-group" role="group">
+                    <a href="" type="button" class="btn btn-default menu_start">ДЕТСКИЙ МИР</a>
+                </div>
+                <div class="btn-group" role="group">
+                    <a href="" type="button" class="btn btn-default menu_start">РАБОТА</a>
+                </div>
+                <div class="btn-group" role="group">
+                    <a href="" type="button" class="btn btn-default menu_start">ЖИВОТНЫЕ</a>
+                </div>
+                <div class="btn-group" role="group">
+                    <a href="" type="button" class="btn btn-default menu_start">ДОМ И САД</a>
+                </div>
+                <div class="btn-group" role="group">
+                    <a href="" type="button" class="btn btn-default menu_start">ЭЛЕКТРОНИКА</a>
+                </div>
+                <div class="btn-group" role="group">
+                    <a href="" type="button" class="btn btn-default menu_start">БИЗНЕС</a>
+                </div>
+                <div class="btn-group" role="group">
+                    <a href="" type="button" class="btn btn-default menu_start">МОДА И СТИЛЬ</a>
+                </div>
+                <div class="btn-group sol" role="group">
+                    <a href="" type="button" class="btn btn-default menu_start">ХОББИ СПОРТ</a>
+                </div>
+                <div class="btn-group sol" role="group">
+                    <a href="" type="button" class="btn btn-default menu_start">НУЖНА ПОМОЩ</a>
+                </div>
+                <div class="btn-group sol" role="group">
+                    <a href="" type="button" class="btn btn-default menu_start">ОТДАМ ДАРОМ</a>
+                </div>
+                <div class="btn-group sol" role="group">
+                    <a href="" type="button" class="btn btn-default menu_start">ОБМЕН</a>
+                </div>
+                <div class="btn-group" role="group" id="more">
+                    <a href="" type="button" class="btn btn-default menu_start">ЕЩЕ</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12" id="sub">
+
         </div>
     </div>
 </div>
--- js/site/menu.js
+++ js/site/menu.js
@@ -1,21 +1,38 @@
 $(document).ready(function () {
     var client_w=document.body.clientWidth;
-
-    var a = $('ul.menu_start').offset().left;
-    var _ul = client_w - 2*170;
-    var _offset = $('ul.menu_start li:nth-child(2)').offset().left - $('ul.menu_start').offset().left - 170;
-    var _count = ~~(_ul/170);
-    _ul = client_w - 2*170 - (_count-1)*_offset;
-    var _count = ~~(_ul/170);
-
-    var _new_offset = (client_w - (_count-1)*_offset - _count*170)/2;
+    var all_width = 0;
+    var all_margin = 48;
+    $('ul.menu_start a').each(function(i,elem) {
+        if ($(this).hasClass("a")) {
+            all_width += $(this).width();
+        }
+    });
+    var _new_offset = ~~(client_w - (all_margin + all_width))/2;
     $('ul.menu_start').offset({left:_new_offset});
 
-    $.ajax({
-        url: 'site/index',
-        method: 'post',
-        dataType: 'json',
-        data: {counts:_count},
+    $('.btn-default.menu_start').hover(function(){
+        $('#sub').css('height',70+'px');
+    },
+        function(){
+            $('#sub').css('height',0+'px');
+        }
+    );
+    $('#more').hide();
+    var client_w=document.body.clientWidth;
+    if(client_w < 1370){
+        $('.sol').hide();
+        $('#more').removeClass('hidden');
+    }
+
+    $(window).resize(function() {
+        var client_w=document.body.clientWidth;
+        if(client_w < 1600){
+            $('.sol').hide();
+            $('#more').show();
+        }
+        if(client_w > 1600){
+            $('.sol').show();
+            $('#more').hide();
+        }
     });
-    alert(_new_offset);
 });
